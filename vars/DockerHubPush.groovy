2e118844e80ee082f0b5f9aadb1effd51fa23b70
Kartik Kain <iamkain1815@gmail.com>
2025-08-09 22:08:55 +0530

    Update DockerHubPush.groovy
    Updated Code

--- vars/DockerHubPush.groovy
+++ vars/DockerHubPush.groovy
@@ -1,8 +1,8 @@
-def call(){
+def call(String Project, String Imagetag, String DockerHubUser){
   echo 'Pushing the Image to Docker Hub Initiated......'
   withCredentials([usernamePassword(credentialsId:'DockerHubCred',usernameVariable:'DockerHubUser',passwordVariable:'DockerHubPass')]){
   sh 'docker login -u $DockerHubUser -p $DockerHubPass'
-  sh 'docker image tag simple-app:latest $DockerHubUser/simple-app:latest'
-  sh 'docker push $DockerHubUser/simple-app:latest'
+  sh 'docker image tag $Project:latest $DockerHubUser/$Project:latest'
+  sh 'docker push $DockerHubUser/$Project:$Imagetag'
   echo '@@@ Image Pushed to Docker Hub Successfully @@@' 
 }
