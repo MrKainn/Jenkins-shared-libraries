2702f82bfb4551253df1ab5b69f1065022953461
Kartik Kain <iamkain1815@gmail.com>
2026-01-21 00:00:13 +0530

    Add docker_build function to build Docker images

---
+++ vars/docker_build.groovy
@@ -0,0 +1,5 @@
+def call(String imagetag, String file_path){
+  echo "Building docker image: ${imagetag}"
+  sh "docker build -t ${imagetag} -f ${file_path} --no-cache"
+  echo "${imagetag} Build sccessfully..."
+}
