17a2a6cd6eebaaa4917febd0d5fe28bbc024a246
Kartik Kain <iamkain1815@gmail.com>
2026-01-21 02:50:10 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,5 @@
 def call(String imagetag, String file_path){
   echo "Building docker image: ${imagetag}"
-  sh "docker build -t ${imagetag} -f ${file_path} --no-cache"
+  sh "docker build -t ${imagetag} -f ${file_path} --no-cache ."
   echo "${imagetag} Build sccessfully..."
 }
